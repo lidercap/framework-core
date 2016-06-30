a45fae5de8599d38e63819db14ead41e7edc337e
Leonardo Thibes <leonardothibes@gmail.com>
2016-06-30 18:43:08 -0300

    componente Core\Type\String

--- src/Type/String.php
+++ src/Type/String.php
@@ -24,4 +24,173 @@ class String implements CoreTypeInterface
     {
         return strlen($this->value);
     }
+
+    /**
+     * Remove espaços do início e do fim da string.
+     *
+     * @return string
+     */
+    public function trim()
+    {
+        return trim($this->value);
+    }
+
+    /**
+     * Converte a string para minúsculas.
+     *
+     * @return string
+     */
+    public function lowercase()
+    {
+        return strtolower($this->value);
+    }
+
+    /**
+     * Converte a string para maiúsculas.
+     *
+     * @return string
+     */
+    public function uppercase()
+    {
+        return strtoupper($this->value);
+    }
+
+    /**
+     * Deixa a string somente com a primeira letra maiúscula.
+     *
+     * @return string
+     */
+    public function capitalize()
+    {
+        return ucfirst(strtolower($this->value));
+    }
+
+    /**
+     * Trunca a string.
+     *
+     * @param int    $limit Tamanho limite da string.
+     * @param string $char  Caracter a ser apendado ao final.
+     *
+     * @return string
+     */
+    public function truncate($limit = 30, $char = '...')
+    {
+        if (strlen($this->value) <= (int)$limit) {
+            return $this->value;
+        }
+
+        return substr($this->value, 0, (int)$limit - strlen($char)) . $char;
+    }
+
+    /**
+     * Garante que o caracter especificado será o primeiro caracter da string.
+     *
+     * @param string $char
+     *
+     * @return string
+     */
+    public function firstChar($char)
+    {
+        return (substr($this->value, 0, 1) !== $char) ? $char . $this->value : $this->value;
+    }
+
+    /**
+     * Garante que o caracter especificado será o último caracter da string.
+     *
+     * @param string $char
+     *
+     * @return string
+     */
+    public function lastChar($char)
+    {
+        return (substr($this->value, -1) !== $char) ? $this->value . $char : $this->value;
+    }
+
+    /**
+     * Remove acentuação e caracteres especiais da string.
+     *
+     * @return string
+     */
+    public function clear()
+    {
+        return strtr(
+            trim($this->value),
+            array(
+
+                /** Bloco do A **/
+                'á' => 'a',
+                'Á' => 'A',
+                'à' => 'a',
+                'À' => 'A',
+                'â' => 'a',
+                'Â' => 'A',
+                'ã' => 'a',
+                'Ã' => 'A',
+                'ä' => 'a',
+                'Ä' => 'A',
+                /** Bloco do A **/
+
+                /** Bloco do E **/
+                'é' => 'e',
+                'É' => 'E',
+                'è' => 'e',
+                'È' => 'E',
+                'ê' => 'e',
+                'Ê' => 'E',
+                'ë' => 'e',
+                'Ë' => 'E',
+                /** Bloco do E **/
+
+                /** Bloco do I **/
+                'í' => 'i',
+                'Í' => 'I',
+                'ì' => 'i',
+                'Ì' => 'I',
+                'î' => 'i',
+                'Î' => 'I',
+                'ĩ' => 'i',
+                'Ĩ' => 'I',
+                'ï' => 'i',
+                'Ï' => 'I',
+                /** Bloco do I **/
+
+                /** Bloco do O **/
+                'ó' => 'o',
+                'Ó' => 'O',
+                'ò' => 'o',
+                'Ò' => 'O',
+                'ô' => 'o',
+                'Ô' => 'O',
+                'õ' => 'o',
+                'Õ' => 'O',
+                'ö' => 'o',
+                'Ö' => 'O',
+                /** Bloco do O **/
+
+                /** Bloco do U **/
+                'ú' => 'u',
+                'Ú' => 'U',
+                'ù' => 'u',
+                'Ù' => 'U',
+                'û' => 'u',
+                'Û' => 'U',
+                'ũ' => 'u',
+                'Ũ' => 'U',
+                'ü' => 'u',
+                'Ü' => 'U',
+                /** Bloco do U **/
+
+                /** Bloco do Ç **/
+                'ç' => 'c',
+                'Ç' => 'C',
+                /** Bloco do Ç **/
+
+                /** Bloco dos ordinais **/
+                'ª' => 'a',
+                'º' => 'o',
+                '°' => 'o',
+                /** Bloco dos ordinais **/
+            )
+        );
+    }
 }
