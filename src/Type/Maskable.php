d16f594a5a614a1ef8cb7d18689c23721dda272a
Leonardo Thibes <leonardothibes@gmail.com>
2016-07-01 10:01:50 -0300

    implementando interface Maskable

--- src/Type/Maskable.php
+++ src/Type/Maskable.php
@@ -1,11 +1,30 @@
 <?php
 
-namespace Lidercap\Core\Behavior;
+namespace Lidercap\Core\Type;
 
 /**
  * Interface que define o uso de máscaras em tipos de dados.
  */
 interface Maskable
 {
+    /**
+     * Verifica se o dados está com máscara.
+     *
+     * @return bool
+     */
+    public function isMasked();
 
+    /**
+     * Mascara o dado.
+     *
+     * @return string
+     */
+    public function mask();
+
+    /**
+     * Retira a máscara do dado.
+     *
+     * @return string
+     */
+    public function unMask();
 }
