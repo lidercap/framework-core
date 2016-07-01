5158f855a442d4abbc70e43076c0bc586a74f4a6
Leonardo Thibes <leonardothibes@gmail.com>
2016-07-01 11:14:21 -0300

    Componente Core\Type\Cpf

--- src/Type/Cpf.php
+++ src/Type/Cpf.php
@@ -7,7 +7,7 @@ use Respect\Validation\Validator as v;
 /**
  * Core Type CPF.
  */
-class Cpf extends Number
+class Cpf extends Number implements Maskable
 {
     /**
      * @return bool
@@ -15,5 +15,48 @@ class Cpf extends Number
     public function isValid()
     {
         return v::cpf()->validate($this->value);
+    }
+
+    /**
+     * Verifica se o dados está com máscara.
+     *
+     * @return bool
+     */
+    public function isMasked()
+    {
+        return (bool)preg_match('/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/', $this->value);
+    }
+
+    /**
+     * Mascara o dado.
+     *
+     * @return string
+     */
+    public function mask()
+    {
+        $cpf = trim($this->value);
+
+        return substr($cpf, 0, 3) . '.' .
+               substr($cpf, 3, 3) . '.' .
+               substr($cpf, 6, 3) . '-' .
+               substr($cpf, 9, 2);
+    }
+
+    /**
+     * Retira a máscara do dado.
+     *
+     * @return string
+     */
+    public function unMask()
+    {
+        return str_replace(array('.', '-'), '', trim($this->value));
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorMessage()
+    {
+        return 'CPF inválido';
     }
 }
--- tests/Type/CpfTest.php
+++ tests/Type/CpfTest.php
@@ -6,18 +6,174 @@ use Lidercap\Core\Type\Cpf;
 
 class CpfTest extends \PHPUnit_Framework_TestCase
 {
-    public function testIsValidMasked()
+    /**
+     * @return array
+     */
+    public function providerValidCpfs()
     {
-        new Cpf('450.945.879-78');
+        return [
+            ['897.478.455-63'],
+            ['89747845563'],
+            [89747845563],
+
+            ['353.584.816-48'],
+            ['35358481648'],
+            [35358481648],
+
+            ['894.254.992-68'],
+            ['89425499268'],
+            [89425499268],
+        ];
     }
 
-    public function testIsValidUnMaskedString()
+    /**
+     * @dataProvider providerValidCpfs
+     *
+     * @param string $cpf
+     */
+    public function testIsValid($cpf)
     {
-        new Cpf('45094587978');
+        new Cpf($cpf);
     }
 
-    public function testIsValidUnMaskedNumber()
+    /**
+     * @return array
+     */
+    public function providerInvalidCpfs()
     {
-        new Cpf(45094587978);
+        return [
+            [''],
+            ['111.111.111-11'],
+            ['11111111111'],
+            ['222.222.222-22'],
+            ['22222222222'],
+            ['333.333.333-33'],
+            ['33333333333'],
+            ['444.444.444-44'],
+            ['44444444444'],
+            ['555.555.555-55'],
+            ['55555555555'],
+            ['666.666.666-66'],
+            ['66666666666'],
+            ['777.777.777-77'],
+            ['77777777777'],
+            ['888.888.888-88'],
+            ['88888888888'],
+            ['999.999.999-99'],
+            ['99999999999'],
+            ['000.000.000-00'],
+            ['00000000000'],
+            ['897.478.455-64'],
+            ['89747845564'],
+            ['353.584.816-49'],
+            ['35358481649'],
+            ['894.254.992-69'],
+            ['89425499269'],
+            ['897.478.455-64'],
+            ['89747845564'],
+            ['353.584.816-49'],
+            ['35358481649'],
+            ['894.254.992-69'],
+            ['89425499269'],
+        ];
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage CPF inválido
+     * @expectedExceptionCode -1
+     *
+     * @dataProvider providerInvalidCpfs
+     *
+     * @param string $cpf
+     */
+    public function testIsInvalid($cpf)
+    {
+        new Cpf($cpf);
+    }
+
+    /**
+     * @return array
+     */
+    public function providerValidMaskedCpfs()
+    {
+        return [
+            ['897.478.455-63'],
+            ['353.584.816-48'],
+            ['894.254.992-68'],
+        ];
+    }
+
+    /**
+     * @dataProvider providerValidMaskedCpfs
+     *
+     * @param string $value
+     */
+    public function testIsMaked($value)
+    {
+        $cpf = new Cpf($value);
+        $this->assertTrue($cpf->isMasked());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerValidUnMaskedCpfs()
+    {
+        return [
+            ['89747845563'],
+            [89747845563],
+            ['35358481648'],
+            [35358481648],
+            ['89425499268'],
+            [89425499268],
+        ];
+    }
+
+    /**
+     * @dataProvider providerValidUnMaskedCpfs
+     *
+     * @param string $value
+     */
+    public function testIsUnMaked($value)
+    {
+        $cpf = new Cpf($value);
+        $this->assertFalse($cpf->isMasked());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerMasks()
+    {
+        return [
+            ['897.478.455-63', '89747845563'],
+            ['353.584.816-48', '35358481648'],
+            ['894.254.992-68', '89425499268'],
+        ];
+    }
+
+    /**
+     * @dataProvider providerMasks
+     *
+     * @param string $masked
+     * @param string $unMasked
+     */
+    public function testMask($masked, $unMasked)
+    {
+        $cpf = new Cpf($unMasked);
+        $this->assertEquals($masked, $cpf->mask());
+    }
+
+    /**
+     * @dataProvider providerMasks
+     *
+     * @param string $masked
+     * @param string $unMasked
+     */
+    public function testUnMask($masked, $unMasked)
+    {
+        $cpf = new Cpf($masked);
+        $this->assertEquals($unMasked, $cpf->unMask());
     }
 }
