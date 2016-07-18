ff85cc0be5fd9244e5852ab1aeebfdf620b32b28
Leonardo Thibes <leonardothibes@gmail.com>
2016-07-18 09:37:27 -0300

    Criando interface ConnectionInfoInterface

--- src/Connection/ConnectionInfoInterface.php
+++ src/Connection/ConnectionInfoInterface.php
@@ -7,5 +7,64 @@ namespace Lidercap\Core\Connection;
  */
 interface ConnectionInfoInterface
 {
+    /**
+     * @codeCoverageIgnore
+     *
+     * @return string
+     */
+    public function getApplication();
 
+    /**
+     * @codeCoverageIgnore
+     *
+     * @param string $application the application
+     */
+    public function setApplication($application);
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @return string
+     */
+    public function getServer();
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @param string $server the server
+     */
+    public function setServer($server);
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @return string
+     */
+    public function getDatabase();
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @param string $database the database
+     */
+    public function setDatabase($database);
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @return string
+     */
+    public function getIp();
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @param string $ip the ip
+     */
+    public function setIp($ip);
+
+    /**
+     * @param array $info
+     */
+    public function setConnectionInfo(array $info);
 }
