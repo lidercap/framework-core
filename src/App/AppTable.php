8f1b99d1dcab89433c1d346bacc3c33d75d61e13
Leonardo Thibes <leonardothibes@gmail.com>
2016-07-19 09:51:30 -0300

    removendo o vfsStream

---
+++ src/App/AppTable.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Lidercap\Core\App;
+
+/**
+ * Consulta nomes de apps.
+ */
+class AppTable
+{
+    /**
+     * Tabela com o nomes dos apps.
+     *
+     * @var array
+     */
+    protected static $apps = [
+        'telesena-warmup'  => 'warmup',
+        'telesena-backend' => 'manager',
+    ];
+
+    /**
+     * Converte o nome do composer para o nome do app.
+     *
+     * @param string $composerName
+     *
+     * @return string|false
+     */
+    public static function get($composerName)
+    {
+        return isset(self::$apps[$composerName]) ? self::$apps[$composerName] : false;
+    }
+
+    /**
+     * Converte o nome do app para o nome do composer.
+     *
+     * @param string $appName
+     *
+     * @return string|false
+     */
+    public static function getComposer($appName)
+    {
+        foreach (self::$apps as $composerName => $app) {
+            if ($appName == $app) {
+                return $composerName;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Obtém o nome do app corrente.
+     *
+     * @param string $file
+     *
+     * @return string|false
+     */
+    public function getCurrent($file = null)
+    {
+        if (is_null($file)) {
+            $file = __DIR__ . '/../../../vendor/composer.json';
+        }
+
+        if (!is_file($file)) {
+            return false;
+        }
+
+        $contents = file_get_contents($file);
+        $contents = json_decode($contents, true);
+
+        if (!isset($contents['name'])) {
+            return false;
+        }
+
+        return self::get($contents['name']);
+    }
+}
---
+++ tests/App/AppTableTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Lidercap\Tests\Core\App;
+
+use Lidercap\Core\App\AppTable;
+
+class AppTableTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var string
+     */
+    protected $file;
+
+    public function setUp()
+    {
+        $this->file = __DIR__ . '/../../composer.json';
+    }
+
+    /**
+     * @return array
+     */
+    public function providerApps()
+    {
+        return [
+            ['telesena-warmup' , 'warmup'],
+            ['telesena-backend', 'manager'],
+        ];
+    }
+
+    /**
+     * @dataProvider providerApps
+     *
+     * @param string $composerName
+     * @param string $appName
+     */
+    public function testGetSuccess($composerName, $appName)
+    {
+        $this->assertEquals($appName, AppTable::get($composerName));
+    }
+
+    public function testGetFail()
+    {
+        $this->assertFalse(AppTable::get('invalid'));
+    }
+
+    /**
+     * @dataProvider providerApps
+     *
+     * @param string $composerName
+     * @param string $appName
+     */
+    public function testGetComposerSuccess($composerName, $appName)
+    {
+        $this->assertEquals($composerName, AppTable::getComposer($appName));
+    }
+
+    public function testGetComposerFail()
+    {
+        $this->assertFalse(AppTable::getComposer('invalid'));
+    }
+}
