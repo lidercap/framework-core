59dbf9fce268a5f3e5b0b4ad843afb3cddf0167f
Leonardo Thibes <leonardothibes@gmail.com>
2017-03-22 10:53:33 -0300

    Implementando a classe CpfUtil

---
+++ src/Util/CpfUtil.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Lidercap\Core\Util;
+
+/**
+ * Classe proxy para "Lidercap\Core\Type\CpfType".
+ *
+ * @method bool   isValid(string $cpf)
+ * @method bool   isMasked(string $cpf)
+ * @method string mask(string $cpf)
+ * @method string unMask(string $cpf)
+ * @method int    length(string $cpf)
+ */
+class CpfUtil extends AbstractUtil
+{
+
+}
