f0271e9f9356beb1708f5690b14e3b48787aed73
Leonardo Thibes <leonardothibes@gmail.com>
2017-03-22 09:50:34 -0300

    Implementando classe ArrayUtil

---
+++ src/Util/ArrayUtil.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Lidercap\Core\Util;
+
+/**
+ * Classe proxy para "Lidercap\Core\Type\ArrayType".
+ *
+ * @method bool isValid(array $array)
+ * @method int  length(array $array)
+ */
+class ArrayUtil extends AbstractUtil
+{
+
+}
