9a1f23586f2733863b5ebe584755720b28d630de
Leonardo Thibes <leonardothibes@gmail.com>
2017-03-24 16:55:29 -0300

    Criando classe de proxy "UrlUtil"

---
+++ src/Util/UrlUtil.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lidercap\Core\Util;
+
+/**
+ * Classe proxy para "Lidercap\Core\Type\UrlType".
+ *
+ * @method bool   isValid(string $string) static
+ * @method string (string $url) static
+ * @method string getScheme(string $url) static
+ * @method string getUser(string $url) static
+ * @method string getPass(string $url) static
+ * @method string getHost(string $url) static
+ * @method string getPort(string $url) static
+ * @method string getPath(string $url) static
+ * @method string getQuery(string $url) static
+ * @method string getAnchor(string $url) static
+ * @method string parse(string $url) static
+ */
+class UrlUtil extends AbstractUtil
+{
+
+}
